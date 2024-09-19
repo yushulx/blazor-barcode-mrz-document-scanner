8caf39bd091279f501f153ef88628ece06d24e56
yushulx <lingxiao1002@gmail.com>
2024-09-19 15:01:14 +0800

    Add support for MRZ recognition

---
+++ VisionTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorCaptureVision
+{
+    public class VisionTypes
+    {
+        public static string BARCODE = "barcode";
+        public static string MRZ = "mrz";
+        public static string DOCUMENT = "document";
+    }
+}
