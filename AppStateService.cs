73c80762e1e05326ad2cfabc3ecd2527e18d404c
yushulx <lingxiao1002@gmail.com>
2024-10-24 16:10:09 +0800

    Add Dynamic Web TWAIN REST API sample

---
+++ AppStateService.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorCaptureVision
+{
+    public class AppStateService
+    {
+        // One-day trial license key
+        public string LicenseKey { get; set; } = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
+
+        public event Action OnChange;
+
+        public void SetData(string data)
+        {
+            LicenseKey = data;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
--- wwwroot/jsInterop.js
+++ wwwroot/jsInterop.js
@@ -284,21 +284,22 @@ window.jsFunctions = {
         if (isInitialized) return true;
 
         try {
-            Dynamsoft.Core.CoreModule.engineResourcePaths = {
-                std: "https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-std@1.2.10/dist/",
-                dip: "https://cdn.jsdelivr.net/npm/dynamsoft-image-processing@2.2.30/dist/",
-                core: "https://cdn.jsdelivr.net/npm/dynamsoft-core@3.2.30/dist/",
-                license: "https://cdn.jsdelivr.net/npm/dynamsoft-license@3.2.21/dist/",
-                cvr: "https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-router@2.2.30/dist/",
-                dce: "https://cdn.jsdelivr.net/npm/dynamsoft-camera-enhancer@4.0.3/dist/",
-                dbr: "https://cdn.jsdelivr.net/npm/dynamsoft-barcode-reader@10.2.10/dist/",
-                dlr: "https://cdn.jsdelivr.net/npm/dynamsoft-label-recognizer@3.2.30/dist/",
-                dcp: "https://cdn.jsdelivr.net/npm/dynamsoft-code-parser@2.2.10/dist/",
-                ddn: "https://cdn.jsdelivr.net/npm/dynamsoft-document-normalizer@2.2.10/dist/",
-                dnn: "https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-dnn@1.0.20/dist/",
-                dlrData: "https://cdn.jsdelivr.net/npm/dynamsoft-label-recognizer-data@1.0.11/dist/",
-            };
-            Dynamsoft.DDV.Core.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-document-viewer@latest/dist/engine";
+            Dynamsoft.Core.CoreModule.engineResourcePaths.rootDirectory = "https://cdn.jsdelivr.net/npm";
+            //Dynamsoft.Core.CoreModule.engineResourcePaths = {
+            //    std: "https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-std@1.2.10/dist/",
+            //    dip: "https://cdn.jsdelivr.net/npm/dynamsoft-image-processing@2.2.30/dist/",
+            //    core: "https://cdn.jsdelivr.net/npm/dynamsoft-core@3.2.30/dist/",
+            //    license: "https://cdn.jsdelivr.net/npm/dynamsoft-license@3.2.21/dist/",
+            //    cvr: "https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-router@2.2.30/dist/",
+            //    dce: "https://cdn.jsdelivr.net/npm/dynamsoft-camera-enhancer@4.0.3/dist/",
+            //    dbr: "https://cdn.jsdelivr.net/npm/dynamsoft-barcode-reader@10.2.10/dist/",
+            //    dlr: "https://cdn.jsdelivr.net/npm/dynamsoft-label-recognizer@3.2.30/dist/",
+            //    dcp: "https://cdn.jsdelivr.net/npm/dynamsoft-code-parser@2.2.10/dist/",
+            //    ddn: "https://cdn.jsdelivr.net/npm/dynamsoft-document-normalizer@2.2.10/dist/",
+            //    dnn: "https://cdn.jsdelivr.net/npm/dynamsoft-capture-vision-dnn@1.0.20/dist/",
+            //    dlrData: "https://cdn.jsdelivr.net/npm/dynamsoft-label-recognizer-data@1.0.11/dist/",
+            //};
+            //Dynamsoft.DDV.Core.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-document-viewer@latest/dist/engine";
             
             Dynamsoft.License.LicenseManager.initLicense(license, true);
 
@@ -310,7 +311,7 @@ window.jsFunctions = {
             await Dynamsoft.DDV.Core.init();
 
             Dynamsoft.DWT.ResourcesPath = "dist";
-            Dynamsoft.DWT.ProductKey = license;
+            //Dynamsoft.DWT.ProductKey = license;
 
             isInitialized = true;
         } catch (e) {
